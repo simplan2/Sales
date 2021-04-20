685fb4bdf43900232dfd43324bd7b208da54e1ae
simplan2 <68392104+simplan2@users.noreply.github.com>
2021-04-20 11:53:53 -0500

    commit Backend y Api

---
+++ Sales/Sales.Backend/Models/LocalDataContext.cs
@@ -0,0 +1,10 @@
+﻿namespace Sales.Backend.Models
+{
+    using Domain.Models;
+
+
+    public class LocalDataContext: DataContext
+    {
+        public System.Data.Entity.DbSet<Sales.Common.Models.Product> Products { get; set; }
+    }
+}
